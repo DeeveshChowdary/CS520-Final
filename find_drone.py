71a4ac77ad4190aa5b9f560f354f2ccb2a08ddf1
Deevesh Chowdary <42311355+DeeveshChowdary@users.noreply.github.com>
2022-12-21 14:05:09 -0500

    base version

--- find_drone.py
+++ find_drone.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import copy
 import heapq
-import math
+from queue import deque
 
 '''
 To Do : 
@@ -10,12 +10,15 @@ To Do :
 Check and change performAction
 Change logic in Astar
 Change heuristics to get different length sequence
+Change manhattan distance to bfs
 
+change code for bfs
 '''
 
 
 class Finder:
     def __init__(self):
+        self.distanceMatrix = None
         return
 
     def check(self, grid):
@@ -41,7 +44,59 @@ class Finder:
 
         return hash(tuple(lst))
 
-    def compute_heuristic(self, probs):
+    def bfs(self, destination, grid):
+
+        queue = deque()
+
+        queue.append((destination, 0))
+
+        visited = set()
+        visited.add(destination)
+
+        rows = [-1, 1, 0, 0]
+        cols = [0, 0, -1, 1]
+        path = [[-1 for i in range(len(grid[0]))] for j in range(len(grid))]
+        directions = ["DOWN", "UP", "RIGHT", "LEFT"]
+
+        while queue:
+            elem = queue.popleft()
+            row = elem[0][0]
+            col = elem[0][1]
+            for i in range(4):
+                newRow = row + rows[i]
+                newCol = col + cols[i]
+
+                if (
+                    0 <= newRow < len(grid)
+                    and 0 <= newCol < len(grid[0])
+                    and (newRow, newCol) not in visited
+                    and grid[newRow][newCol] != -1
+                ):
+                    visited.add((newRow, newCol))
+                    path[newRow][newCol] = elem[1] + 1
+                    queue.append(((newRow, newCol), elem[1] + 1))
+
+        return path
+
+    def fourMatrix(self, grid):
+        movi = [
+            [
+                [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
+                for k in range(len(grid[0]))
+            ]
+            for l in range(len(grid))
+        ]
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                path = self.bfs((i, j), grid)
+                for k in range(len(path)):
+                    for l in range(len(path[0])):
+                        movi[i][j][k][l] = path[k][l]
+
+        self.distanceMatrix = movi
+
+    def compute_heuristic(self, probs, actions):
         heuristic = 0
 
         #one of the heuristic I am using is the max distance between any two nodes with non-zero probability.
@@ -56,10 +111,14 @@ class Finder:
                         for q in range(len(probs[0])):
                             if probs[p][q] > 0:
                                 
-                                #distance between these two nodes 
-
+                                #distance between these two nodes
+                                if not self.distanceMatrix:
+                                    
+                                    self.fourMatrix(self.probs) 
+                                
+                                manhattan = self.distanceMatrix[p][q][i][j]
                                 # calculate manhattan distance
-                                manhattan = abs(p - i) + abs(q - j)
+                                # manhattan = abs(p - i) + abs(q - j)
 
                                 d = max(d, manhattan)
 
@@ -72,7 +131,7 @@ class Finder:
                     ctr += 1
         
         #add weights to heuristic
-        return (50 * maxdist) * (5 * ctr)
+        return ( 80 * (maxdist ) ) * ((10 * ctr)) * ( len(actions))
 
     def performAction(self, grid, action=[]):
         if type(action) == list:
@@ -95,13 +154,37 @@ class Finder:
 
             return newGrid
 
+    def testActions( self, probs, actions):
+        print("before: ")
+        self.display(probs)
+        newprobs = copy.deepcopy(probs)
+        print(actions)
+        for action in actions:
+            
+            if action == "UP":
+                newGrid = self.move_up(newprobs)
+            elif action == "DOWN":
+                newGrid = self.move_down(newprobs)
+            elif action == "LEFT":
+                newGrid = self.move_left(newprobs)
+            elif action == "RIGHT":
+                newGrid = self.move_right(newprobs)
+            else:
+                newGrid = copy.deepcopy(newprobs)
+
+            newprobs = copy.deepcopy(newGrid)
+        print("after: ")
+        self.display(newGrid)
+        
+        return newGrid
+
     def astar(self):
 
         heap = []
         self.actions = ["UP", "DOWN", "LEFT", "RIGHT"]
         probs = copy.deepcopy(self.probs)
 
-        h = self.compute_heuristic(probs)
+        h = self.compute_heuristic(probs,[])
 
         heap.append((h, [], probs))
 
@@ -112,6 +195,7 @@ class Finder:
             elem = heapq.heappop(heap)
 
             if self.check(elem[2]):
+                # print(elem[2])
                 return elem[1]
 
             actions = elem[1]
@@ -132,7 +216,7 @@ class Finder:
 
                 newprobs = self.performAction(probs, act)
 
-                newHeu = self.compute_heuristic(newprobs)
+                newHeu = self.compute_heuristic(newprobs,actions)
 
                 if hash(self.convertToTuple(newprobs)) not in visited:
                     visited.add(hash(self.convertToTuple(newprobs)))
@@ -230,4 +314,11 @@ if __name__ == "__main__":
 
     f.load_grid()
 
+    # print(f.probs)
+    
+    # newprobs = f.move_down(f.probs)
+    # print(newprobs)
     print(f.find_drone())
+    # print(f.probs)
+    # actions = ['LEFT', 'LEFT', 'UP', 'UP', 'UP', 'UP', 'LEFT', 'LEFT', 'LEFT', 'UP', 'UP', 'LEFT', 'LEFT', 'UP', 'UP', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'RIGHT', 'UP', 'LEFT', 'UP', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'UP', 'LEFT', 'DOWN', 'DOWN', 'RIGHT', 'DOWN', 'DOWN', 'DOWN', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'UP', 'UP', 'UP', 'RIGHT', 'UP', 'LEFT', 'UP', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'DOWN', 'DOWN', 'DOWN', 'DOWN', 'RIGHT', 'DOWN', 'DOWN', 'LEFT', 'DOWN', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'LEFT', 'UP', 'RIGHT', 'RIGHT', 'UP', 'UP', 'UP', 'UP', 'UP', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'UP', 'RIGHT', 'DOWN', 'DOWN', 'LEFT', 'UP', 'LEFT', 'LEFT', 'DOWN', 'RIGHT', 'RIGHT', 'UP', 'LEFT', 'UP', 'UP', 'UP', 'UP', 'UP', 'LEFT', 'UP', 'RIGHT', 'UP', 'RIGHT', 'UP', 'LEFT', 'DOWN', 'LEFT', 'UP', 'LEFT', 'DOWN', 'DOWN', 'RIGHT', 'RIGHT', 'RIGHT', 'DOWN', 'DOWN', 'DOWN', 'DOWN', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'RIGHT', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP', 'UP']
+    # f.testActions(f.probs,actions)
